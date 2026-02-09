62fc91bcd2d84aa17a0cb175de865035b4f16419
TechNing39 <leeyujineee@gmail.com>
2026-02-09 23:25:44 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+
+        StringBuilder sb=new StringBuilder();
+        sb.append(a+b);
+        System.out.print(sb.toString());
+    }
+}

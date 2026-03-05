9da58c50cfeecd0471ebaffde659e1f9fa90a7f7
TechNing39 <leeyujineee@gmail.com>
2026-03-05 21:17:41 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+N=int(input())
+num=list(map(int,input().split()))
+
+count=0
+
+for i in num:
+    if i<2:
+        continue
+
+    is_prime=True
+    for j in range(2,i):
+        if i%j==0:
+            is_prime=False
+            break
+
+    if is_prime:
+        count+=1
+
+print(count)
